0a198aef3bdc9afe74837eb7a7af9e9e4bcadaec
yaroslav beskhlibnyi <beskhlibnyi@gmail.com>
2026-06-11 18:10:26 +0300

    Add forge mode

--- app/controllers/arena_controller.rb
+++ app/controllers/arena_controller.rb
@@ -4,6 +4,11 @@ class ArenaController < ApplicationController
     @selected_keys = []
   end
 
+  def forge
+    @roster = DuelSimulator.roster
+    @selected_keys = []
+  end
+
   def simulate
     fighter_keys = [params[:fighter_a], params[:fighter_b]]
     render json: DuelSimulator.new(selected_keys: fighter_keys).simulate
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   root "arena#show"
+  get "forge" => "arena#forge"
   post "duel/simulate" => "arena#simulate"
 end
